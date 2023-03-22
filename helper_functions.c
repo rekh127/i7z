88e00847cc7b38c85113fcb9b6a59aaff5bec860
Daniel Butler <rekh127@gmail.com>
2023-03-21 20:03:29 -0700

    Update helper_functions.c
    change to correctly identify which cores are part of which sockets/physical processors. At least on my system. 
    
    might look into processing `sysctl kern.sched.topology_spec`   to guarantee correctness

--- helper_functions.c
+++ helper_functions.c
@@ -821,7 +821,7 @@ void construct_CPU_Hierarchy_info(struct cpu_hierarchy_info* chi)
         }
         for (i = 0; i < ncpu; i++) {
             processor_num = i;
-            if ((coreid_num + 1) == cores)
+            if ((coreid_num + 1) == threads)
                 physicalid_num++;
             physicalid_num %= packages;
             coreid_num = processor_num % cores;
